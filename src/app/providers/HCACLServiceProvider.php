956e03e273ea9b56d604fc317d05ec39ee7a0c86
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-02-06 10:41:49 +0200

    Formatting

--- src/app/providers/HCACLServiceProvider.php
+++ src/app/providers/HCACLServiceProvider.php
@@ -21,35 +21,35 @@ class HCACLServiceProvider extends ServiceProvider
     /**
      * Bootstrap the application services.
      */
-    public function boot()
+    public function boot ()
     {
         // register artisan commands
-        $this->commands($this->commands);
+        $this->commands ($this->commands);
 
         // loading views
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'HCACL');
+        $this->loadViewsFrom (__DIR__ . '/../resources/views', 'HCACL');
 
         // loading translations
-        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'HCACL');
+        $this->loadTranslationsFrom (__DIR__ . '/../resources/lang', 'HCACL');
 
         // registering elements to publish
-        $this->registerPublishElements();
+        $this->registerPublishElements ();
 
         // registering helpers
-        $this->registerHelpers();
+        $this->registerHelpers ();
 
         // registering routes
-        $this->registerRoutes();
+        $this->registerRoutes ();
     }
 
     /**
      * Register helper function
      */
-    private function registerHelpers()
+    private function registerHelpers ()
     {
         $filePath = __DIR__ . '/../Http/helpers.php';
 
-        if (\File::isFile($filePath)) {
+        if (\File::isFile ($filePath)) {
             require_once $filePath;
         }
     }
@@ -60,22 +60,22 @@ class HCACLServiceProvider extends ServiceProvider
     private function registerPublishElements ()
     {
         // Publish your migrations
-        $this->publishes([
-            __DIR__ . '/../../database/migrations/' => database_path('/migrations'),
+        $this->publishes ([
+            __DIR__ . '/../../database/migrations/' => database_path ('/migrations'),
         ], 'migrations');
 
         // Publishing assets
-        $this->publishes([
-            __DIR__ . '/../public' => public_path('honeycomb'),
+        $this->publishes ([
+            __DIR__ . '/../public' => public_path ('honeycomb'),
         ], 'public');
     }
 
     /**
      * Registering routes
      */
-    private function registerRoutes()
+    private function registerRoutes ()
     {
-        \Route::group(['namespace' => $this->namespace], function ($router) {
+        \Route::group (['namespace' => $this->namespace], function ($router) {
             require __DIR__ . '/../../app/honeycomb/routes.php';
         });
     }
