4fcd6206ca46e93e4eacda6e0c228d776aa7d9f2
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-02-07 11:26:46 +0200

    Added register routes

--- src/app/honeycomb/routes.php
+++ src/app/honeycomb/routes.php
@@ -54,27 +54,29 @@ Route::group (['prefix' => 'auth', 'middleware' => ['web']], function () {
     Route::get ('login', ['as' => 'auth.login', 'middleware' => 'guest', 'uses' => 'HCAuthController@showLogin']);
     Route::post ('login', ['as' => 'auth.login', 'uses' => 'HCAuthController@login']);
 
+    Route::get('register', ['as' => 'auth.register', 'middleware' => 'guest', 'uses' => 'HCAuthController@showRegister']);
+    Route::post('register', ['as' => 'auth.register', 'uses' => 'HCAuthController@register']);
+
     Route::get ('logout', ['as' => 'auth.logout', 'middleware' => 'auth', 'uses' => 'HCAuthController@logout']);
 });
 
 //packages/interactivesolutions/honeycomb-acl/src/app/routes/routes.users.php
 
 Route::group (['prefix' => 'admin', 'middleware' => ['web', 'auth']], function () {
-    Route::get ('users', ['as' => 'admin.users', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_list'], 'uses' => '\\HCUsersController@adminView']);
+    Route::get ('users', ['as' => 'admin.users', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_list'], 'uses' => 'HCUsersController@adminView']);
 
     Route::group (['prefix' => 'api'], function () {
-        Route::get ('users', ['as' => 'admin.api.users', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_list'], 'uses' => '\\HCUsersController@listData']);
-        Route::get ('users/search', ['as' => 'admin.api.users.search', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_list'], 'uses' => '\\HCUsersController@search']);
-        Route::get ('users/{id}', ['as' => 'admin.api.users.single', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_list'], 'uses' => '\\HCUsersController@getSingleRecord']);
-        Route::post ('users/{id}/duplicate', ['as' => 'admin.api.users.duplicate', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_update'], 'uses' => '\\HCUsersController@duplicate']);
-        Route::post ('users/restore', ['as' => 'admin.api.users.restore', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_update'], 'uses' => '\\HCUsersController@restore']);
-        Route::post ('users/merge', ['as' => 'admin.api.users.merge', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_update'], 'uses' => '\\HCUsersController@merge']);
-        Route::post ('users', ['middleware' => ['acl:interactivesolutions_honeycomb_acl_users_create'], 'uses' => '\\HCUsersController@create']);
-        Route::put ('users/{id}', ['middleware' => ['acl:interactivesolutions_honeycomb_acl_users_update'], 'uses' => '\\HCUsersController@update']);
-        Route::delete ('users/{id}', ['middleware' => ['acl:interactivesolutions_honeycomb_acl_users_delete'], 'uses' => '\\HCUsersController@delete']);
-        Route::delete ('users', ['middleware' => ['acl:interactivesolutions_honeycomb_acl_users_delete'], 'uses' => '\\HCUsersController@delete']);
-        Route::delete ('users/{id}/force', ['as' => 'admin.api.users.force', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_force_delete'], 'uses' => '\\HCUsersController@forceDelete']);
-        Route::delete ('users/force', ['as' => 'admin.api.users.force.multi', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_force_delete'], 'uses' => '\\HCUsersController@forceDelete']);
+        Route::get ('users', ['as' => 'admin.api.users', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_list'], 'uses' => 'HCUsersController@listData']);
+        Route::get ('users/search', ['as' => 'admin.api.users.search', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_list'], 'uses' => 'HCUsersController@search']);
+        Route::get ('users/{id}', ['as' => 'admin.api.users.single', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_list'], 'uses' => 'HCUsersController@getSingleRecord']);
+        Route::post ('users/{id}/duplicate', ['as' => 'admin.api.users.duplicate', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_update'], 'uses' => 'HCUsersController@duplicate']);
+        Route::post ('users/restore', ['as' => 'admin.api.users.restore', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_update'], 'uses' => 'HCUsersController@restore']);
+        Route::post ('users/merge', ['as' => 'admin.api.users.merge', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_update'], 'uses' => 'HCUsersController@merge']);
+        Route::post ('users', ['middleware' => ['acl:interactivesolutions_honeycomb_acl_users_create'], 'uses' => 'HCUsersController@create']);
+        Route::put ('users/{id}', ['middleware' => ['acl:interactivesolutions_honeycomb_acl_users_update'], 'uses' => 'HCUsersController@update']);
+        Route::delete ('users/{id}', ['middleware' => ['acl:interactivesolutions_honeycomb_acl_users_delete'], 'uses' => 'HCUsersController@delete']);
+        Route::delete ('users', ['middleware' => ['acl:interactivesolutions_honeycomb_acl_users_delete'], 'uses' => 'HCUsersController@delete']);
+        Route::delete ('users/{id}/force', ['as' => 'admin.api.users.force', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_force_delete'], 'uses' => 'HCUsersController@forceDelete']);
+        Route::delete ('users/force', ['as' => 'admin.api.users.force.multi', 'middleware' => ['acl:interactivesolutions_honeycomb_acl_users_force_delete'], 'uses' => 'HCUsersController@forceDelete']);
     });
 });
-
--- src/app/routes/routes.auth.php
+++ src/app/routes/routes.auth.php
@@ -5,5 +5,8 @@ Route::group (['prefix' => 'auth', 'middleware' => ['web']], function () {
     Route::get ('login', ['as' => 'auth.login', 'middleware' => 'guest', 'uses' => 'HCAuthController@showLogin']);
     Route::post ('login', ['as' => 'auth.login', 'uses' => 'HCAuthController@login']);
 
+    Route::get('register', ['as' => 'auth.register', 'middleware' => 'guest', 'uses' => 'HCAuthController@showRegister']);
+    Route::post('register', ['as' => 'auth.register', 'uses' => 'HCAuthController@register']);
+
     Route::get ('logout', ['as' => 'auth.logout', 'middleware' => 'auth', 'uses' => 'HCAuthController@logout']);
 });
