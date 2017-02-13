f8c59a392ffadfd50a9ee3acb91d13deb5ab5fb6
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-02-13 18:47:19 +0200

    Updated translations

--- src/resources/lang/en/acl_roles.php
+++ src/resources/lang/en/acl_roles.php
@@ -1,12 +1,13 @@
 <?php
-    return [
-        'id' => 'Id',
-        'count' => 'Count',
-        'created_at' => 'Created at',
-        'updated_at' => 'Updated at',
-        'deleted_at' => 'Deleted at',
-        'name' => 'Name',
-        'slug' => 'Slug',
-        'role_id' => 'Role id',
-        'permission_id' => 'Permission id',
-    ];
+return [
+    'id'            => 'Id',
+    'count'         => 'Count',
+    'created_at'    => 'Created at',
+    'updated_at'    => 'Updated at',
+    'deleted_at'    => 'Deleted at',
+    'name'          => 'Name',
+    'slug'          => 'Slug',
+    'role_id'       => 'Role id',
+    'permission_id' => 'Permission id',
+    'page_title'    => 'Roles'
+];
