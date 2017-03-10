c732613a9b12706ade7ecf375c7cee87f9a60742
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-03-10 13:18:00 +0200

    Removed duplicate functions

--- src/app/http/controllers/HCUsersController.php
+++ src/app/http/controllers/HCUsersController.php
@@ -18,9 +18,9 @@ class HCUsersController extends HCBaseController
         $config = [
             'title'       => trans ('HCACL::users.page_title'),
             'listURL'     => route ('admin.api.users'),
-    //        'newFormUrl'  => route ('admin.api.form-manager', ['users-new']),
-    //        'editFormUrl' => route ('admin.api.form-manager', ['users-edit']),
-    //        'imagesUrl'   => route ('resource.get', ['/']),
+            'newFormUrl'  => route ('admin.api.form-manager', ['users-new']),
+            'editFormUrl' => route ('admin.api.form-manager', ['users-edit']),
+            'imagesUrl'   => route ('resource.get', ['/']),
             'headers'     => $this->getAdminListHeader (),
         ];
 
@@ -170,38 +170,6 @@ class HCUsersController extends HCBaseController
         $list = $this->orderData($list, $select);
 
         return $list;
-    }
-
-    /**
-     * Creating data list
-     * @return mixed
-     */
-    public function pageData()
-    {
-        return $this->createQuery()->paginate($this->recordsPerPage);
-    }
-
-    /**
-     * Creating data list based on search
-     * @return mixed
-     */
-    public function search()
-    {
-        if (!request('q'))
-            return [];
-
-        //TODO set limit to start search
-
-        return $this->list();
-    }
-
-    /**
-     * Creating data list
-     * @return mixed
-     */
-    public function list()
-    {
-        return $this->createQuery()->get();
     }
 
     /**
--- src/app/http/controllers/acl/RolesController.php
+++ src/app/http/controllers/acl/RolesController.php
@@ -63,7 +63,7 @@ class RolesController extends HCBaseController
     /**
      * Create item
      *
-     * @param null $data
+     * @param array|null $data
      * @return mixed
      */
     protected function __create(array $data = null)
@@ -156,38 +156,6 @@ class RolesController extends HCBaseController
         $list = $this->orderData($list, $select);
 
         return $list;
-    }
-
-    /**
-     * Creating data list
-     * @return mixed
-     */
-    public function pageData()
-    {
-        return $this->createQuery()->paginate($this->recordsPerPage);
-    }
-
-    /**
-     * Creating data list based on search
-     * @return mixed
-     */
-    public function search()
-    {
-        if (!request('q'))
-            return [];
-
-        //TODO set limit to start search
-
-        return $this->list();
-    }
-
-    /**
-     * Creating data list
-     * @return mixed
-     */
-    public function list()
-    {
-        return $this->createQuery()->get();
     }
 
     /**
