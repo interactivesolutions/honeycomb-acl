a74b468da670730d991d0cefe82db210bc65e69d
Mantas Paulauskas <mantopastas@gmail.com>
2017-09-08 11:57:45 +0300

    added lt translations

---
+++ src/resources/lang/lt/acl_permissions.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'page_title' => 'Leidimai',
+    'id'         => 'Id',
+    'count'      => 'Numeris',
+    'created_at' => 'Sukurta',
+    'updated_at' => 'Atnaujinta',
+    'deleted_at' => 'Ištrinta',
+    'name'       => 'Pavadinimas',
+    'controller' => 'Kontroleris',
+    'action'     => 'Veiksmas',
+];
---
+++ src/resources/lang/lt/acl_roles.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'id'            => 'Id',
+    'count'         => 'Numeris',
+    'created_at'    => 'Sukurta',
+    'updated_at'    => 'Atnaujinta',
+    'deleted_at'    => 'Ištrinta',
+    'name'          => 'Pavadinimas',
+    'slug'          => 'Slug',
+    'role_id'       => 'Rolės id',
+    'permission_id' => 'Leidimo id',
+    'page_title'    => 'Rolės'
+];
